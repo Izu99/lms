b30f43201a91cb43ae9ac815b43c660e7707ad4d
izu99 <isuruumanga123@gmail.com>
2025-08-07 13:55:03 +0530

    login register

--- server/src/models/Video.ts
+++ server/src/models/Video.ts
@@ -3,7 +3,7 @@ import mongoose, { Document, Types } from 'mongoose';
 export interface IVideo extends Document {
   title: string;
   description: string;
-  videoUrl: string;
+  videoUrl: string; 
   uploadedBy: Types.ObjectId;
 }
 
