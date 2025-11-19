32e4af529d8dc1088dd25026c22e14edacd0d937
izu99 <isuruumanga123@gmail.com>
2025-11-19 21:17:49 +0530

    remove vercel file now

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; 
 import { useState, useEffect, useRef } from "react";
 import useDebounce from "@/hooks/useDebounce"; // Import the useDebounce hook
 import axios from "axios";
